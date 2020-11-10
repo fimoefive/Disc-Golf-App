88042ffad16c0083437da6c6102fd012ed193119
fimoefive <brothermj@hotmail.com>
2020-11-10 11:35:07 -0600

    scoreCards and Rank table css changes.

--- src/components/Ranks/RankCard.js
+++ src/components/Ranks/RankCard.js
@@ -11,9 +11,9 @@ export const RankCard = ({ games }) => (
         <div className="game_score">games.id: {games.id}</div>
         <div className="game__user">games.user.id: {games.user.id}</div> */}
         <Table striped border="4">
-            <thead scope="row"><td>Player</td><td>Average Score</td></thead>
+            <thead scope="row"><td>Player</td><td>{games.user.username}</td></thead>
             <tr></tr>
-            <thead scope="row"><td>{games.user.username}</td><td>{games.score}</td></thead>
+            <thead scope="row"><td>Average Score</td><td className="avgScore">{games.score}</td></thead>
         </Table>
     </section>
 );
